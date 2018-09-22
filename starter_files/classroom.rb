577754c788cd9de9a5261a632e334ea0341dcb89
spatel1009 <spatel1009@gmail.com>
2018-09-22 17:43:48 -0400

    late turn in

--- starter_files/classroom.rb
+++ starter_files/classroom.rb
@@ -1,36 +1,43 @@
-# Given a grade_hash, student name, and assignment number, return the score
-# for that student and assignment. Note that Ruby counts arrays from 0, but
-# we are referring to them as 1-10.
+Given a grade_hash, student name, and assignment number, return the score
+for that student and assignment. Note that Ruby counts arrays from 0, but
+we are referring to them as 1-10.
+
+def assignment_score(grade_hash, student_name, assignment_num)
+    grade_hash[student_name][assignment_num-1]
+end
+
+Given a grade_hash and assignment number, return all scores for that
+assignment. Note that Ruby counts arrays from 0, but we are referring to
+them as 1-10.
+def column (grades, col_num
+    output = []
+    grid.each d)
+
+Given a grade_hash and assignment number, return the average score for that
+assignment. Note that Ruby counts arrays from 0, but we are referring to
+them as 1-10.
 
 
-# Given a grade_hash and assignment number, return all scores for that
-# assignment. Note that Ruby counts arrays from 0, but we are referring to
-# them as 1-10.
+Return a hash of students and their average score.
+TIP: To convert an array like [[:indiana, 90], [:nevada, 80]] to a hash,
+use .to_h. Also look at Hash#transform_values.
+def average (students, grades)
+    
 
 
-# Given a grade_hash and assignment number, return the average score for that
-# assignment. Note that Ruby counts arrays from 0, but we are referring to
-# them as 1-10.
+Return a letter grade for a numerical score.
+90+ => A
+80-89 => B
+70-79 => C
+60-69 => D
+< 60 => F
+
+Return a hash of students and their final letter grade, as determined
+by their average.
 
 
-# Return a hash of students and their average score.
-# TIP: To convert an array like [[:indiana, 90], [:nevada, 80]] to a hash,
-# use .to_h. Also look at Hash#transform_values.
+Return the average for the entire class.
 
 
-# Return a letter grade for a numerical score.
-# 90+ => A
-# 80-89 => B
-# 70-79 => C
-# 60-69 => D
-# < 60 => F
-
-# Return a hash of students and their final letter grade, as determined
-# by their average.
-
-
-# Return the average for the entire class.
-
-
-# Return an array of the top `number_of_students` students.
+Return an array of the top `number_of_students` students.
 
